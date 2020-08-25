8aef67e9d9e518a8b4fa6abe7d49c05621b613f3
alxayeed <alxayeed@gmail.com>
2020-08-25 21:03:49 +0600

    Mongodb Atlas db configured using mongoose.connection sucessful

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql").graphqlHTTP;
 const schema = require("./schema/schema");
+const mongoose = require("mongoose", { useUnifiedTopology: true });
+const dotenv = require("dotenv");
 
 const app = express();
 
@@ -14,5 +16,24 @@ app.use(
 );
 
 app.listen(4000, () => {
-  console.log(typeof graphqlHTTP);
+  console.log("Listening to port 4000 for connections");
+});
+
+//connect to mongodb Atlas database
+
+dotenv.config();
+
+const mongoAtlasString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.zjljy.mongodb.net/<dbname>?retryWrites=true&w=majority`;
+
+mongoose.connect(mongoAtlasString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("error", function (error) {
+  console.log(error);
+});
+
+mongoose.connection.once("open", () => {
+  console.log("Successfully connected to MongoDB Atlas");
 });
